df1f36ac6e2587f7a69e5bf7d03f2cfcfa324cd0
Navya <navyaraj103@gmail.com>
2026-06-22 01:30:23 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import os
+
 
 app = Flask(
     __name__,
@@ -35,4 +37,5 @@ def recommend():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
